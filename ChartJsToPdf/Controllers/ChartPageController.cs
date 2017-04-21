8f4800ac5a121f9b250dcb43ced348b19d43703a
AmithS <kdashivantha@gmail.com>
2017-04-21 16:22:23 +0530

    initial commit

---
+++ ChartJsToPdf/Controllers/ChartPageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChartJsToPdf.Controllers
+{
+    public class ChartPageController : Controller
+    {
+        // GET: ChartPage
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ChartJsToPdf/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChartJsToPdf.Startup))]
+namespace ChartJsToPdf
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
